93abff8d04a3c4e2bdc45601fb8f7e194d0e856d
sianya <sianyidgm@gmail.com>
2024-02-22 17:53:14 -0600

    añadido un sout como ejemplo de programacion

--- src/HW2.java
+++ src/HW2.java
@@ -2,5 +2,7 @@ public class HW2 {
     public static void main(String[] args) throws Exception {
         // comprobando que funciona
         System.out.println("Hola Sianya :D, éste es un modo muy curioso de decir hola");
+
+        System.out.println("Aquí se realizaria la creacion y programacion de nuestro proyecto");
     }
 }
