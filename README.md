d0852080152f0dc414c81b3322a4e34ffbe5de7f
sianya <sianyidgm@gmail.com>
2024-02-22 17:51:24 -0600

    Se han realizado pequeños cambios

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 La Tarea 2 de POO requiere la creación de un repositorio remoto para colaborar en el con un compañero. 
+
+Efectivamente, no solo eso, tambien requiere altos conocimientos en git y gitHub.
--- src/HW2.java
+++ src/HW2.java
@@ -1,5 +1,6 @@
-public class App {
+public class HW2 {
     public static void main(String[] args) throws Exception {
+        // comprobando que funciona
         System.out.println("Hola Sianya :D, éste es un modo muy curioso de decir hola");
     }
 }
